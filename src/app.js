3aab5830c8e12e8c233cf30b5c1cf53ad35c665b
akshar2603 <aksharlakhankiua@gmail.com>
2024-08-02 21:17:08 +0530

    under const

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const app = express(); 
 const hbs = require('hbs') ;
 const Register = require('./models/register') ;
+const jwt = require('jsonwebtoken') ;
+ require('dotenv').config() ; 
 
 const port = process.env.PORT || 8000 ; 
 const static_path = path.join(__dirname, '../public'); 
 const temp_path = path.join(__dirname, "../templates/views"); 
 const partials_path = path.join(__dirname, "../templates/partials"); 
-
+const bcrypt = require('bcrypt');
 console.log(static_path)
 
 hbs.registerPartials(partials_path) ;
@@ -33,6 +35,9 @@ app.post('/register', async(req, res) => {
         password :req.body.password
     }) ;
 
+    const token = await reg.generateAuthToken() ;
+    console.log(token) ;
+
     const registered = await reg.save(); 
     res.status(201).render('index'); 
 
@@ -47,19 +52,78 @@ app.get('/', async (req,res) => {
     // res.send(result) ;
 }) ;
 app.get('/register', async (req,res) =>{
-    // res.send("ya i am calling") ;
-    const { mail, password } = req.query;
-    const email = mail ; 
-
-    const fetchmail =  await Register.find({ email } , 'firstname');
-    const fetchpassword = await Register.find({ password} , 'firstname');
-    // console.log(fetchpassword); 
-    // console.log(fetchmail); 
-    if(fetchmail.firstname == fetchpassword.firstname){
-        res.send("login successful") ;
-    }
+   
+    
+    res.render('register') ;
 })
+// for validation 
+
+app.post('/login', async(req,res) =>{
+        try{
+            const email = req.body.email; 
+            const password = req.body.password; 
+
+            //console.log(email + " " + password) ;
+          const useremail =  await  Register.findOne({email:email}) ; 
+
+          const isMatch = await bcrypt.compare(password, useremail.password) ;
+        //   res.send(useremail) ;
+        //   console.log(useremail) ;
+        
+
+        const token = await useremail.generateAuthToken() ;
+        console.log(token) ;
+        if(isMatch){
+            res.send("login successful") ;
+            console.log("login successful") ;
+        }
+        else res.render('index') ;
+
+
+        }
+        catch(e){
+            console.log("enter valid mail and password") ;
+        }
+
+})
+// lets do something awesome here we are doing encryption and decryption to our databases 
+
+// in db its in encrypt  for and in our cde we decrypt is 
+
+// for that we have bcrypt package take 3 year to decript for haacker 
+
+const securePass = async(password) => {
+    const passwordHash = await bcrypt.hash(password, 10) ; // here 10 is for rounds according to our algo takes time for compile and give more security
+    const passwordMatch = await bcrypt.compare(password, passwordHash) ;
+    //console.log(passwordHash) ;
+    // console.log(passwordMatch);
+}
+securePass("akshar");
+
+
+// how its work 
+
+// const jwt = require('jsonwebtoken') ;
+
+// const createToken = async() => {
+//     const token = await jwt.sign({_id:"66a9282ea8a631b9267b746a"}, "mynameisaksharjogdsgnsofshdfsdofhassdhfhdsifauffi");
+//     console.log(token) ; // its give three part second part is your body part 
+
+//     const userVer = await jwt.verify(token , "mynameisaksharjogdsgnsofshdfsdofhassdhfhdsifauffi");
+//     console.log(userVer) ;
+// }
+// createToken() ;
+
+
+
 app.listen(port , () =>{
     console.log(`server is running on  ${port}`) ;
 })
+
+
+
+
+
+//authentocation atle ak var daroke amaxon ma login karyu atle biji var pacggu no karvu pade amuk log out no karo tya sudhi chene awasome \
+// for that we use json web token means server gives use unique token atle a request sathe add j hoi token 
 
--- src/models/register.js
+++ src/models/register.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose') ;
-
+const bcrypt = require('bcrypt') ;
+const jwt = require('jsonwebtoken') ;
 const emloySchema = new mongoose.Schema({
     firstname : {
         type : String, 
@@ -18,8 +19,44 @@ const emloySchema = new mongoose.Schema({
     password :{
         type : String, 
         required:true
-    }
+    },
+    tokens:[{
+        token:{
+            type : String, 
+            required:true
+        }
+    }]
 })
+
+//generating tokens 
+
+emloySchema.methods.generateAuthToken = async function(){
+    try{
+        console.log(this._id) ;
+        const key = process.env.SECRET_KEY ;
+        const token = jwt.sign({_id:this._id.toString()}, key);
+           
+        this.tokens = this.tokens.concat({token:token}) ;
+
+        await this.save() ;
+        return token ;
+    }
+    catch(err){
+        console.log(err); 
+    }
+}
+
+// this is also one type of middleware 
+
+
+emloySchema.pre("save", async function(next){
+    // console.log(`current password is  ${this.password}`) ;
+    if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10) ; 
+    } // here check if password is going to be modified or not 
+    next() ; // here in our case its a save method next vala piece of code run hojayega 
+})
+
 
 const Register = new mongoose.model("Register", emloySchema) ;
 
